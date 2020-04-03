298d4d411256941f391f57f6348498021095aff7
fabrizio.magma <fv@magma.consulting>
2020-04-03 11:33:50 +0200

    Lesson 11

--- MageMastery/Todo/view/frontend/web/js/view/todo.js
+++ MageMastery/Todo/view/frontend/web/js/view/todo.js
@@ -1,5 +1,14 @@
 define(['uiComponent'],function() {
     'use strict';
-    console.log("todo");
+    //console.log("todo");
+
     return Component.extend({});
+    /* we could insert template configuration here instead of in phtml file
+    return Component.extend({
+        defaults: {
+
+            template: 'MageMastery_Todo/todo'
+        }
+    });
+    */
 });
