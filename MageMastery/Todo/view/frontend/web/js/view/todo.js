2fd3866b293d28cd8a6fc8ca2cdd1e455b56894f
fabrizio.magma <fv@magma.consulting>
2020-04-04 13:35:52 +0200

    fixed missing argument from Lesson 10

--- MageMastery/Todo/view/frontend/web/js/view/todo.js
+++ MageMastery/Todo/view/frontend/web/js/view/todo.js
@@ -1,7 +1,6 @@
-define(['uiComponent'],function() {
+define(['uiComponent'],function(Component) {
     'use strict';
     //console.log("todo");
-
     return Component.extend({});
     /* we could insert template configuration here instead of in phtml file
     return Component.extend({
