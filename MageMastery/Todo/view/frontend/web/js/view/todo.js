66b290612134e69c39450a5aea653f0969002c8a
fabrizio.magma <fv@magma.consulting>
2020-04-09 22:21:06 +0200

    Lesson 17

--- MageMastery/Todo/view/frontend/web/js/view/todo.js
+++ MageMastery/Todo/view/frontend/web/js/view/todo.js
@@ -1,13 +1,19 @@
 define(['uiComponent'],function(Component) {
     'use strict';
     //console.log("todo");
-    return Component.extend({});
-    /* we could insert template configuration here instead of in phtml file
     return Component.extend({
         defaults: {
-
-            template: 'MageMastery_Todo/todo'
-        }
+            tasks: [
+                {label: "Task 1"},
+                {label: "Task 2"},
+                {label: "Task 3"},
+                {label: "Task 4"}
+            ]
+        },
+        initObservable: function () {
+            this._super().observe(['tasks']);
+            this.tasks().push({label:"Task 5"});
+            return this;
+        },
     });
-    */
 });
