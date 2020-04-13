b1cba1a602e73afb67e5e7b1c1f312eb7b5ffe67
fabrizio.magma <fv@magma.consulting>
2020-04-13 15:57:08 +0200

    Lesson 21

--- MageMastery/Todo/view/frontend/web/js/view/todo.js
+++ MageMastery/Todo/view/frontend/web/js/view/todo.js
@@ -1,14 +1,29 @@
-define(['uiComponent'],function(Component) {
+define(['uiComponent','jquery'],function(Component,$) {
     'use strict';
     //console.log("todo");
     return Component.extend({
         defaults: {
             tasks: [
-                {label: "Task 1"},
-                {label: "Task 2"},
-                {label: "Task 3"},
-                {label: "Task 4"}
+                {id:1, label: "Task 1", status:false},
+                {id:2, label: "Task 2", status:false},
+                {id:3, label: "Task 3", status:false},
+                {id:4, label: "Task 4", status:true}
             ]
+        },
+        initObservable: function () {
+            this._super().observe(['tasks']);
+            return this;
+        },
+
+        switchStatus: function (data, event) {
+            const taskId= $(event.target).data('id');
+            var items = this.tasks().map(function(task){
+                if(task.id===taskId) {
+                    task.status= !task.status;
+                }
+                return task;
+            });
+            this.tasks(items);
         },
     });
 });
