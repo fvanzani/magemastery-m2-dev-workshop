5896bb19211865be5725c0cd2d372851ca99e297
fabrizio.magma <fv@magma.consulting>
2020-04-21 16:10:09 +0200

    Lesson 32

--- MageMastery/Todo/Controller/Index/Index.php
+++ MageMastery/Todo/Controller/Index/Index.php
@@ -2,13 +2,36 @@
 namespace MageMastery\Todo\Controller\Index;
 
 use Magento\Framework\App\Action\Action;
+use Magento\Framework\App\Action\Context;
 use Magento\Framework\Controller\ResultFactory;
+use MageMastery\Todo\Model\Task;
+use MageMastery\Todo\Model\ResourceModel\Task as TaskResource;
+use MageMastery\Todo\Model\TaskFactory;
+
 
 class Index extends Action
 {
+    private $taskResource;
+    private $taskFactory;
+
+    public function __construct(Context $context, TaskFactory $taskFactory, TaskResource $taskResource)
+    {
+        $this->taskFactory = $taskFactory;
+        $this->taskResource = $taskResource;
+
+        parent::__construct($context);
+    }
 
     public function execute()
     {
+        $task = $this->taskFactory->create();
+        $task->setData([
+            'label' => 'New Task 22',
+            'status' => 'open',
+            'customer_id' => 1
+        ]);
+        $this->taskResource->save($task);
+
         return $this->resultFactory->create(ResultFactory::TYPE_PAGE);
     }
 }
